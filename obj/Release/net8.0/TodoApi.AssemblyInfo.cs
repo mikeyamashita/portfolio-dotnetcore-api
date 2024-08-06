b12174e5a1dbb32445912c45f7ddb16398c48c7f
Mike Yamashita <mrmyamashita@gmail.com>
2024-08-06 11:28:25 -0600

    release build

--- obj/Release/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Release/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ff89b5d7d39f0fa7be7843f08ad807bc269ae0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b54f0da46ad41ef8eae21f77a18545ab9348237")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
